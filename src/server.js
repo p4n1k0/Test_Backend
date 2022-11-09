bd08d60d0af93cc7c0afa3ae213c9365dab43e0b
Tarick <gabre.tarick@gmail.com>
2022-11-08 21:27:21 -0300

    inital commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const connection = require('./db/connection');
+const port = 3001;
+
+app.listen(port, async () => {
+  console.log(`API Teste_Backend está sendo executada na porta ${port}`);
+
+  // O código abaixo é para testa a comunicação com o MySQL
+  const [ result ] = await connection.execute('SELECT 1');
+
+  if (result) {
+    console.log('MySQL connection OK');
+  }
+});
