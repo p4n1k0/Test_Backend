920b75a664db9e7a53433c1bb04a0890137481c6
Tarick <gabre.tarick@gmail.com>
2022-11-10 13:21:18 -0300

    arqv de criacao do banco

---
+++ src/utils/schema.sql
@@ -0,0 +1,23 @@
+DROP SCHEMA IF EXISTS Teste_Backend;
+CREATE SCHEMA Teste_Backend;
+USE Teste_Backend;
+CREATE TABLE usuarios (
+id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(255) NOT NULL,
+sobrenome VARCHAR(255) NOT NULL,
+email VARCHAR(255) UNIQUE NOT NULL,
+telefone VARCHAR(45) NOT NULL,
+cpf VARCHAR(45) UNIQUE NOT NULL
+);
+CREATE TABLE enderecos_usuario ( 
+id_endereco_usuario INT AUTO_INCREMENT PRIMARY KEY,
+logradouro VARCHAR(255) NOT NULL,
+numero VARCHAR(45) NOT NULL,
+cidade VARCHAR(255) NOT NULL,
+uf VARCHAR(2) NOT NULL,
+cep VARCHAR(45) NOT NULL,
+bairro VARCHAR(255) NOT NULL,
+complemento VARCHAR(20) NOT NULL,
+id_usuario INT NOT NULL,
+FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+);
